5d53cab6866b855c49856b75bf76221b4aa92ea2
k.abramkin <k.abramkin@kernel.ua>
2024-01-23 01:11:04 +0200

    add ui libs and multiple pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts}"],
+  content: ["./index.html", "./pages/**/*.{html,js,ts}", "./src/**/*.{js,ts}"],
   theme: {
     extend: {},
   },
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/vite-practice/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "pages/about.html"),
+      },
+    },
+  },
 });
